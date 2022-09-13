6a730b168d46fd09d9aa6a4fedfe9e5334f5603e
John Fuhrmeister <61069716+JohnFuhrm12@users.noreply.github.com>
2022-09-13 00:02:55 -0400

    Add files via upload

--- src/Homescreen.js
+++ src/Homescreen.js
@@ -25,11 +25,11 @@ function Homescreen( {...props} ) {
         <>
         <div className='page'>
             <div className='titleBar'>
-                <h1 className='titleName' onClick={refresh} >JF Aquatics</h1>
-                <h1 className='catName'>Corals</h1>
-                <h1 className='catName'>Supplies</h1>
-                <h1 className='catName'>Fish</h1>
-                <h1 className='catName'>Inverts</h1>
+                    <h1 className='titleName' onClick={refresh} >JF Aquatics</h1>
+                    <h1 className='catName'>Corals</h1>
+                    <h1 className='catName'>Supplies</h1>
+                    <h1 className='catName'>Fish</h1>
+                    <h1 className='catName'>Inverts</h1>
                 <div className='searchCart'>
                     <img src={search} className="search" alt="Search"/>
                     <input onChange={(e) => {setSearchKey(e.target.value)}} onKeyDown={(e) => {searchFunc(e)}} className='searchBar' type="text" value={searchKey} placeholder="Search ..."/>
